e0c3f9201eb23c6ed794185a60848dfe01b57af2
Alexandr <saint12maloj@gmail.com>
2021-01-20 01:58:08 +0300

    Added comments

--- Kalyuganov.Demo.HttpClient/RestClient.cs
+++ Kalyuganov.Demo.HttpClient/RestClient.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -17,7 +18,32 @@ namespace Kalyuganov.Demo.Http
             _httpClient = httpClient;
         }
 
-        public async Task<T> Post<T>(string domain, string endpoint, T resource)
+        // return await _httpClient.GetOne<User>(domain, endpoint);
+        public async Task<T> GetOne<T>(string domain, string endpoint)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"{domain}/{endpoint}");
+
+            var response = await Send(httpRequest);
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<T>(stringResult);
+
+            return result;
+        }
+
+        // return await _client.GetMany<Users>(domain, endpoint);
+        public async Task<IEnumerable<T>> GetMany<T>(string domain, string endpoint)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"{domain}/{endpoint}");
+
+            var response = await Send(httpRequest);
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<IEnumerable<T>>(stringResult);
+
+            return result;
+        }
+
+        // return await _client.Post<User>(domain, endpoint, user);
+        public async Task<T> Post<T>(string domain, string endpoint, T resource)     
         {
             var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"{domain}/{endpoint}");
 
@@ -31,12 +57,37 @@ namespace Kalyuganov.Demo.Http
             return result;
         }
 
+        // return await _client.Put<User, UserPatch>(domain, endpoint, patch);
+        public async Task<R> Put<R, T>(string serviceUrl, string endpoint, T patch)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Put, $"{serviceUrl}/{endpoint}");
+
+            string body = JsonConvert.SerializeObject(patch);
+
+            httpRequest.Content = new StringContent(body, Encoding.UTF8, "application/json");
+            var response = await Send(httpRequest);
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<R>(stringResult);
+
+            return result;
+        }
+
+        // Task<Unit>
+        //   await _httpClient.Delete(domain, endpoint);
+        // return Unit.Value;
+        public Task Delete(string domain, string endpoint)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Delete, $"{domain}/{endpoint}");
+            return Send(httpRequest);
+        }
+
         protected async Task<HttpResponseMessage> Send(HttpRequestMessage httpRequest)
         {
             var response = await _httpClient.SendAsync(httpRequest);
 
             if (response.IsSuccessStatusCode)
                 return response;
+
             else
             {
                 var responseString = await response.Content.ReadAsStringAsync();
